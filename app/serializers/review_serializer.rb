4e6b9d15946cdd45d9b96a7bab302e860dff4807
github username <github email address>
2019-07-01 13:06:45 -0600

    fix gemfile

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :human_name, :pet_type, :pet_name, :content, :created_at
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-Picture.create(image: '../public/uploads/img1', description: "Volunteering at the Denver Cat Cafe")
+Review.create(human_name: "Amy", pet_type: "dog", pet_name: "Sir Barks-A-Lot", content: "Sir Barks-A-Lot didn't want to come home!")
+Review.create(human_name: "Sylvia", pet_type: "bird", pet_name: "Tweeta", content: "Tweeta loves to snuggle with Keilana!")
+Review.create(human_name: "Michael", pet_type: "dragon", pet_name: "FireFace", content: "FireFace had a great time playing in Keilana's backyard!")
+Review.create(human_name: "Ella", pet_type: "fish", pet_name: "Bubbly", content: "Bubbly loves the attention she gets from Keilana!")
+Review.create(human_name: "Jess", pet_type: "dog", pet_name: "Hank", content: "Hank says Keilana takes him for the best walks!")
+Review.create(human_name: "Lyla", pet_type: "cat", pet_name: "Poptart", content: "Poptart has never been happier!")
+Review.create(human_name: "Ashley", pet_type: "dog", pet_name: "Mr. Grumps", content: "Mr. Grumps says Keilana is the best petsitter ever!")
+Review.create(human_name: "Alex", pet_type: "dragon", pet_name: "Hot Cheeto", content: "Keilana is Hot Cheeto's favorite petsitter!")
+Review.create(human_name: "Matt", pet_type: "bird", pet_name: "Frank", content: "Keilana always teaches Frank new tricks!")
+
+Event.create(human_name: "Israel", pet_name: "Sir Barks-A-Lot", start: "2019-06-07", end: "2019-06-11")
+Event.create(human_name: "Xander", pet_name: "Mr. Grumps", start: "2019-05-29", end: "2019-06-02")
