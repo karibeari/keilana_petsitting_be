28276ad00c148d353360c25187b5fcac7a9bd7ad
github username <klferenczy@gmail.com>
2019-08-03 00:18:01 -0600

    update serializer for events

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,3 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :start, :end, :human_name, :pet_name
+  attributes :start, :end, :human_name, :pet_name, :pet_type, :email, :phone, :confirmed
 end
