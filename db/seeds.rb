6f3c4f6330a3e2b1cf021272e4193a0741dbe5a3
github username <klferenczy@gmail.com>
2019-08-03 00:07:26 -0600

    fix seed file phone numbers

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@ Review.create(human_name: "Ashley", pet_type: "dog", pet_name: "Mr. Grumps", con
 Review.create(human_name: "Alex", pet_type: "dragon", pet_name: "Hot Cheeto", content: "Keilana is Hot Cheeto's favorite petsitter!")
 Review.create(human_name: "Matt", pet_type: "bird", pet_name: "Frank", content: "Keilana always teaches Frank new tricks!")
 
-Event.create(human_name: "Israel", pet_name: "Sir Barks-A-Lot", start: "2019-06-07", end: "2019-06-11", pet_type: "large", email: "test@gmail.com", phone: 5554258431)
-Event.create(human_name: "Xander", pet_name: "Mr. Grumps", start: "2019-05-29", end: "2019-06-02", pet_type: "small", email: "test@gmail.com", phone: 5554258431)
+Event.create(human_name: "Israel", pet_name: "Sir Barks-A-Lot", start: "2019-06-07", end: "2019-06-11", pet_type: "large", email: "test@gmail.com")
+Event.create(human_name: "Xander", pet_name: "Mr. Grumps", start: "2019-05-29", end: "2019-06-02", pet_type: "small", email: "test@gmail.com")
